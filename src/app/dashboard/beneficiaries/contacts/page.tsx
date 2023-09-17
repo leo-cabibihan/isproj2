c405d833f47627edf37118a052f62c05864942f5
Leo <leocabibihan@outlook.com>
2023-09-17 21:13:09 +0800

    scuffed add form example plz polish

--- src/app/dashboard/beneficiaries/contacts/page.tsx
+++ src/app/dashboard/beneficiaries/contacts/page.tsx
@@ -1,49 +1,87 @@
 import { Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@/components/Table"
+import SlideOver from "@/components/SlideOverButton"
+import { TextField } from "@/components/Fields"
+import { Button } from "@/components/Button"
 
 export default function Page() {
-    return (
-        <>
-            <TableContainer>
-                <Table>
-                    <Thead>
-                        <Tr>
-                            <Th>To convert</Th>
-                            <Th>into</Th>
-                            <Th>multiply by</Th>
-                            <Th> </Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        <Tr>
-                            <Td>inches</Td>
-                            <Td>millimetres (mm)</Td>
-                            <Td>25.4</Td>
-                            <Td>
-                                <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                    Edit<span className="sr-only">, kek</span>
-                                </a>
+  return (
+    <>
+      <SlideOver buttonText="bruh">
+        <form className="space-y-6" action="#" method="POST">
+          <TextField
+            label="Email Address"
+            name="email"
+            type="email"
+            autoComplete="email"
+            required
+          />
 
-                            </Td>
-                        </Tr>
-                        <Tr>
-                            <Td>inches</Td>
-                            <Td>millimetres (mm)</Td>
-                            <Td>25.4</Td>
-                        </Tr>
-                        <Tr>
-                            <Td>feet</Td>
-                            <Td>centimetres (cm)</Td>
-                            <Td>30.48</Td>
-                        </Tr>
-                        <Tr>
-                            <Td>yards</Td>
-                            <Td>metres (m)</Td>
-                            <Td>0.91444</Td>
-                        </Tr>
-                    </Tbody>
-                </Table>
-            </TableContainer>
-        </>
+          <TextField
+            label="Password"
+            name="password"
+            type="password"
+            autoComplete="current-password"
+            required
+          />
 
-    )
+          <div className="flex items-center justify-between">
+            <div className="text-sm leading-6">
+              <a href="/forgot" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                Forgot password?
+              </a>
+            </div>
+          </div>
+
+          <div className="col-span-full">
+            <Button type="submit" variant="solid" color="blue" className="w-full">
+              <span>
+                Sign up <span aria-hidden="true">&rarr;</span>
+              </span>
+            </Button>
+          </div>
+        </form>
+      </SlideOver>
+      <TableContainer>
+        <Table>
+          <Thead>
+            <Tr>
+              <Th>To convert</Th>
+              <Th>into</Th>
+              <Th>multiply by</Th>
+              <Th> </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>inches</Td>
+              <Td>millimetres (mm)</Td>
+              <Td>25.4</Td>
+              <Td>
+                <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                  Edit<span className="sr-only">, kek</span>
+                </a>
+
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>inches</Td>
+              <Td>millimetres (mm)</Td>
+              <Td>25.4</Td>
+            </Tr>
+            <Tr>
+              <Td>feet</Td>
+              <Td>centimetres (cm)</Td>
+              <Td>30.48</Td>
+            </Tr>
+            <Tr>
+              <Td>yards</Td>
+              <Td>metres (m)</Td>
+              <Td>0.91444</Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </TableContainer>
+    </>
+
+  )
 }
