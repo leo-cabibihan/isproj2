ee2a22df5b7e47c5bf1aee74c0ca301a380bcc48
Ryan Lim <ryanclim2002@yahoo.com>
2023-09-22 20:26:05 +0800

    Supabase integration, also intial C & R in CRUD

--- src/app/dashboard/beneficiaries/contacts/page.tsx
+++ src/app/dashboard/beneficiaries/contacts/page.tsx
@@ -2,14 +2,50 @@ import { Table, TableContainer, TableContent, TableHeaderButton, Tbody, Td, Th, 
 import SlideOver from "@/components/SlideOverButton"
 import { TextField } from "@/components/Fields"
 import { Button } from "@/components/Button"
+import supabase from "@/app/utils/supabase"
+import { revalidatePath } from "next/cache"
 
-export default function Page() {
+export const revalidate = 0;
+
+export default async function Page() {
+  const { data: contacts } = await supabase.from('contacts').select("*").order('id', {ascending: true});
+  
+  const handleSubmit = async (formData: FormData) => {
+    'use server'
+    const beneficiary = {
+      name: formData.get("beneficiary"),
+      contact_no: formData.get("contact_no"),
+      address: formData.get("address")
+    };
+  
+    await supabase.from('contacts').insert(beneficiary);
+    revalidatePath('/');
+  };
+
+  const saveChanges= async (formData: FormData) => {
+    'use server'
+    const contactId = formData.get("id")
+    const beneficiary = {
+      name: formData.get("beneficiary"),
+      contact_no: formData.get("contact_no"),
+      address: formData.get("address")
+    };
+   await supabase.from('contacts')
+    .update({
+      name: beneficiary.name,
+      contact_no: beneficiary.contact_no,
+      address: beneficiary.address
+      })
+      .eq("id", contactId)
+    revalidatePath('/');
+  };
+
   return (
     <>
       <TableContainer>
         <TableHeaderButton header="Contacts">
           <SlideOver buttontext="Add Contact" variant="solid" color="blue">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" action={handleSubmit} method="POST">
               <TextField
                 label="Beneficiary Name"
                 name="beneficiary"
@@ -58,44 +94,67 @@ export default function Page() {
           <Table>
             <Thead>
               <Tr>
-                <Th>To convert</Th>
-                <Th>into</Th>
-                <Th>multiply by</Th>
-                <Th> </Th>
+                <Th>Name</Th>
+                <Th>Contact Number</Th>
+                <Th>Address</Th>
+                <Th>Actions</Th>
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
-                <Td>inches</Td>
-                <Td>millimetres (mm)</Td>
-                <Td>25.4</Td>
-                <Td>
-                  <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                    Edit<span className="sr-only">, kek</span>
-                  </a>
-
-                </Td>
-              </Tr>
-              <Tr>
-                <Td>inches</Td>
-                <Td>millimetres (mm)</Td>
-                <Td>25.4</Td>
-              </Tr>
-              <Tr>
-                <Td>feet</Td>
-                <Td>centimetres (cm)</Td>
-                <Td>30.48</Td>
-              </Tr>
-              <Tr>
-                <Td>yards</Td>
-                <Td>metres (m)</Td>
-                <Td>0.91444</Td>
-              </Tr>
+              {contacts?.map(contact => (
+                <Tr key={contact.id}>
+                  <Td>{contact.name}</Td>
+                  <Td>{contact.contact_no}</Td> 
+                  <Td>{contact.address}</Td>
+                  <Td>
+                  <SlideOver buttontext="Edit Contact" variant="solid" color="blue">
+                    <form key={contact.id} className="space-y-6" action={saveChanges} method="PUT">
+                        <>
+                        <TextField
+                          label=""
+                          name="id"
+                          type="hidden"
+                          defaultValue={contact.id}
+                          required
+                        />
+                          <TextField
+                          label="Beneficiary Name"
+                          name="beneficiary"
+                          type="text"
+                          defaultValue={contact.name}
+                          required
+                        />    
+                          <TextField
+                          label="Contact Number"
+                          name="contact_no"
+                          type="number"
+                          defaultValue={contact.contact_no}
+                          required
+                        />                  
+                          <TextField
+                          label="Address"
+                          name="address"
+                          type="text"
+                          defaultValue={contact.address}
+                          required
+                        />   
+                        </>                                
+                      <div className="col-span-full">
+                        <Button type="submit" variant="solid" color="blue" className="w-full">
+                          <span>
+                            Save Changes <span aria-hidden="true">&rarr;</span>
+                          </span>
+                        </Button>
+                      </div>
+                    </form>
+                  </SlideOver>
+                  </Td>
+                </Tr>
+              ))}
             </Tbody>
           </Table>
         </TableContent>
       </TableContainer>
     </>
-
   )
 }
