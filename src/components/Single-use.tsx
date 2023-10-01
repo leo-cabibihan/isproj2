9d9c629efe0a0cad816d4c2eeed25efe7549e0b8
SleepyGabe <gaboparinas78@gmail.com>
2023-10-01 19:17:30 +0800

    Started on WIP Cash Donation logic. Will explain issue in next meeting.

--- src/components/Single-use.tsx
+++ src/components/Single-use.tsx
@@ -228,15 +228,15 @@ export async function Causes({ id }) {
               </div>
               <div className="max-w-xl">
                 <div className="mt-8 flex items-center gap-x-4 text-xs">
-                  <time dateTime={event.datetime} className="text-gray-500">
-                    {event.date}
+                  <time dateTime={event.start_date} className="text-gray-500">
+                    {event.start_date}
                   </time>
                 </div>
                 <div className="group relative">
                   <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
                     <a href={'/' + event.id + '/events'}>
                       <span className="absolute inset-0" />
-                      {event.title}
+                      {event.name}
                     </a>
                   </h3>
                   <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{event.description}</p>
