a3cd7f956a9cf7f6958eacded07ba8ebf8a502d0
Myko <myko.macawiwili@benilde.edu.ph>
2023-10-07 20:07:20 +0800

    push

--- src/app/onboarding/page.tsx
+++ src/app/onboarding/page.tsx
@@ -11,7 +11,6 @@ import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
 
 
-
 export default function Example() {
   const Changes = async (formData: FormData) => {
     'use server'
