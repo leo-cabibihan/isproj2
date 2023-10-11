9194954cbca592c39f1066b76cd5a02f821ef365
SleepyGabe <gaboparinas78@gmail.com>
2023-10-11 10:43:41 +0800

    minor revisions

--- src/app/dashboard/logs/complaints/page.tsx
+++ src/app/dashboard/logs/complaints/page.tsx
@@ -1,13 +1,16 @@
+import supabase from "@/app/utils/supabase";
+import { Button } from "@/components/Button";
+import { TextField } from "@/components/Fields";
+import SlideOver from "@/components/SlideOverButton";
 import { Table, TableContainer, TableContent, TableHeader, Tbody, Td, Th, Thead, Tr } from "@/components/Table";
+import { revalidatePath } from "next/cache";
 
-const complaints = [
-    { id: "1", DonorName: 'Myko Macawiwili', Charity: 'Red Cross', Date: 'June 16, 2023' },
-    { id: "2", DonorName: 'Gabe Parinas', Charity: 'Red Cross', Date: 'June 13, 2023' },
-    { id: "3", DonorName: 'Leo Cabibihan', Charity: 'Red Cross', Date: 'June 10, 2023' },
-    { id: "4", DonorName: 'Ryan Lim', Charity: 'Tulong Lasalyano', Date: 'June 1, 2023' },
-]
+export const revalidate = 0;
 
-export default function Page() {
+export default async function Page() {
+
+    const { data: complaints } = await supabase.from('donor_complaints').select('*, charity ( id, name ), donor ( id, name )').eq('charity_id', 12)
+
     return (
         <>
             <div className="sm:flex sm:items-center py-9">
@@ -27,16 +30,56 @@ export default function Page() {
                             </Tr>
                         </Thead>
                         <Tbody>
-                            {complaints.map(complaint =>
+                            {complaints?.map(complaint =>
 
                                 <Tr key={complaint.id}>
-                                    <Td>{complaint.DonorName}</Td>
-                                    <Td>{complaint.Charity}</Td>
-                                    <Td>{complaint.Date}</Td>
+                                    <Td>{complaint.donor.name}</Td>
+                                    <Td>{complaint.chariy.name}</Td>
+                                    <Td>{complaint.created_at}</Td>
                                     <Td>
-                                        <a href="#" className="text-indigo-600 hover:text-indigo-900">
-                                            Edit<span className="sr-only">, kek</span>
-                                        </a>
+                                        <SlideOver variant="solid" color="blue" buttontext="View Details">
+                                            <form className="space-y-6" action="#" method="POST">
+                                                <TextField
+                                                    label="Charity Name"
+                                                    name="name"
+                                                    type="text"
+                                                    placeholder={complaint.charity.name}
+                                                    readOnly
+                                                />
+
+                                                <TextField
+                                                    label="Complainant"
+                                                    name="donor"
+                                                    type="text"
+                                                    placeholder={complaint.donor.name}
+                                                    readOnly
+                                                />
+
+                                                <div className="col-span-full">
+                                                    <label htmlFor="reason" className="block text-sm font-medium leading-6 text-gray-900">
+                                                        Details
+                                                    </label>
+                                                    <div className="mt-2">
+                                                        <textarea
+                                                            id="reason"
+                                                            name="reason"
+                                                            rows={3}
+                                                            readOnly
+                                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                            placeholder={complaint.complaint}
+                                                        />
+                                                    </div>
+                                                </div>
+
+                                                <div className="col-span-full">
+                                                    <Button type="submit" variant="solid" color="yellow" className="w-full">
+                                                        <span>
+                                                            Notify Charity <span aria-hidden="true">&rarr;</span>
+                                                        </span>
+                                                    </Button>
+                                                </div>
+                                            </form>
+                                        </SlideOver>
                                     </Td>
                                 </Tr>
 
