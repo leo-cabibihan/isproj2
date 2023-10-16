ea2fe1d1bb74e9e48e4b5133be413f900d7058d0
SleepyGabe <gaboparinas78@gmail.com>
2023-10-16 09:37:52 +0800

    WIP AUTH Middleware

---
+++ src/middleware.ts
@@ -0,0 +1,77 @@
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+  console.log(req)
+
+  const originalUrl = req.nextUrl.protocol + req.headers.get('host') + req.nextUrl.pathname
+  console.log("=========================================> " + originalUrl)
+
+  // console.log('hello i am middleware')
+  // console.log(req.cookies)
+  const supabase = createMiddlewareClient({ req, res })
+  const { data: { user }, error } = await supabase.auth.getUser();
+  // console.log(data, error)
+  console.log(user?.id)
+  const uid = user?.id
+  console.log("UID IS " + uid)
+
+  const { data: donor, error: error_1 } = await supabase
+    .from('donor')
+    .select('*')
+    .eq('id', uid)
+  const { data: charity_member, error: error_2 } = await supabase
+    .from('charity_member')
+    .select('*')
+    .eq('user_uuid', uid)
+  const { data: admin, error: error_3 } = await supabase
+    .from('system_owner')
+    .select('*')
+    .eq('id', uid)
+
+  console.log(donor)
+  console.log(charity_member)
+  console.log(admin)
+
+  // if (donor?.length === 1) {
+  //   NextResponse.redirect('/settings' )
+  // } else if (charity_member?.length === 1) {
+  //   console.log("CHARITY IS CHARITY")
+  // } else if (admin?.length === 1) {
+  //   console.log('FUCK')
+    
+  // }
+
+  if (originalUrl.includes('/admin')) {
+    console.log('ADMIIIIIIIIIN')
+    if (!user || !admin) {
+        console.log("NOT SIGNED IN")
+        return Response.json(
+          { success: false, message: 'authentication failed' },
+          { status: 401 }
+        )
+      }
+  }
+  else if (originalUrl.includes('/dashboard')) {
+    console.log('CHARITY')
+    if (!user || !charity_member) {
+      return Response.json(
+        { success: false, message: 'authentication failed' },
+        { status: 401 }
+      )
+      }
+  }
+  else {
+    if (!user || !donor) {
+      return Response.json(
+        { success: false, message: 'authentication failed' },
+        { status: 401 }
+      )
+    }
+  }
+
+  return res
+}
