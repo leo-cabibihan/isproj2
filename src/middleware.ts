4876386782d12f461562a5702491a656ba226813
SleepyGabe <gaboparinas78@gmail.com>
2023-10-19 15:11:28 +0800

    bye bye console logs :'D

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,71 +4,59 @@ import type { NextRequest } from 'next/server'
 
 
 export async function middleware(req: NextRequest) {
-  var charity_status : any
+  var charity_status: any
   const res = NextResponse.next()
-  console.log(req)
 
   const originalUrl = req.nextUrl.protocol + req.headers.get('host') + req.nextUrl.pathname
-  console.log("=========================================> " + originalUrl)
 
-  // console.log('hello i am middleware')
-  // console.log(req.cookies)
   const supabase = createMiddlewareClient({ req, res })
   const { data: { user }, error } = await supabase.auth.getUser();
-  // console.log(data, error)
-  console.log(user?.id)
   const uid = user?.id
-  console.log("UID IS " + uid)
-
-  const { data: donor, error: error_1 } = await supabase
-    .from('donor')
-    .select('*')
-    .eq('id', uid)
-  const { data: charity_member, error: error_2 } = await supabase
-    .from('charity_member')
-    .select('*, charity ( id, charity_verified )')
-    .eq('user_uuid', uid)
-  const { data: admin, error: error_3 } = await supabase
-    .from('system_owner')
-    .select('*')
-    .eq('id', uid)
-
-  console.log(donor)
-  console.log(charity_member)
-  console.log(admin)
-
-  // if (donor?.length === 1) {
-  //   NextResponse.redirect('/settings' )
-  // } else if (charity_member?.length === 1) {
-  //   console.log("CHARITY IS CHARITY")
-  // } else if (admin?.length === 1) {
-  //   console.log('FUCK')
-    
-  // }
 
   if (originalUrl.includes('/admin')) {
     console.log('ADMIIIIIIIIIN')
+
+    //CHECK FOR ADMIN ROLE
+    const { data: admin, error: error_3 } = await supabase
+      .from('system_owner')
+      .select('*')
+      .eq('id', uid)
+    console.log(admin)
+
     if (admin?.length !== 1) {
-        console.log("NOT SIGNED IN")
-        return Response.redirect('http://localhost:3000')
-    
-        
-      }
+      console.log("NOT SIGNED IN")
+      return Response.redirect('http://localhost:3000')
+    }
   }
   else if (originalUrl.includes('/dashboard')) {
     console.log('CHARITY')
 
+    //CHECK FOR CHARITY ROLE
+    const { data: charity_member, error: error_2 } = await supabase
+      .from('charity_member')
+      .select('*, charity ( id, charity_verified )')
+      .eq('user_uuid', uid)
+    console.log(charity_member)
+
+    //CHECK IF CHARITY IS VERIFIED
     charity_member?.map(member => (
       charity_status = member.charity.charity_verified
     ))
     console.log("THE CHARITY STATUS IS: " + charity_status)
-    
-    if (charity_member?.length !== 1 && charity_status == false) {
+
+    if (charity_member?.length !== 1 || charity_status == false) {
       console.log("NOT A CHARITY FUCK OFF")
       return Response.redirect('http://localhost:3000')
-      }
+    }
   }
   else if (originalUrl.includes('/settings')) {
+    //CHECK FOR DONOR ROLE
+    const { data: donor, error: error_1 } = await supabase
+      .from('donor')
+      .select('*')
+      .eq('id', uid)
+    console.log(donor)
+
     if (donor?.length !== 1) {
       console.log("NOT A DONOR FUCK OFF")
       return Response.redirect('http://localhost:3000')
