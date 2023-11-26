aa20e3f08b26c498ea255d66e4a4b7b8fc28310e
SleepyGabe <gaboparinas78@gmail.com>
2023-11-26 19:48:40 +0800

    Minor UI fix to event details form (admin). Event statuses work fine but refreshing gives server-side exceptions.

--- src/app/admin/view-charity/[id]/page.tsx
+++ src/app/admin/view-charity/[id]/page.tsx
@@ -559,12 +559,25 @@ export default async function Organization({ params }: any) {
                                                         Reason for Rejection
                                                     </label>
                                                     <div className="mt-2">
-                                                        <textarea
-                                                            name="reason"
-                                                            rows={4}
-                                                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                                                            placeholder='Lorem Ipsum Dolor Sit Amet...'
-                                                        />
+                                                        {
+                                                            event.rejection_reason ?
+                                                                (
+                                                                    <textarea
+                                                                        name="reason"
+                                                                        rows={4}
+                                                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                                        defaultValue={event.rejection_reason}
+                                                                    />
+                                                                ) :
+                                                                (
+                                                                    <textarea
+                                                                        name="reason"
+                                                                        rows={4}
+                                                                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                                                        placeholder='Lorem Ipsum Dolor Sit Amet...'
+                                                                    />
+                                                                )
+                                                        }
                                                     </div>
                                                 </div>
 
