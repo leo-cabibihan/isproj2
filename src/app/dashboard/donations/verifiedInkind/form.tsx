a33d977ba4ec78fe9e6c2351f360eaa7c5bb839e
SleepyGabe <gaboparinas78@gmail.com>
2023-11-27 20:13:16 +0800

    minor ui fix to verified item details

--- src/app/dashboard/donations/verifiedInkind/form.tsx
+++ src/app/dashboard/donations/verifiedInkind/form.tsx
@@ -490,18 +490,19 @@ export function EditForm({ id, orgID }: { id: number, orgID: any }) {
                     readOnly
                 />
                 <br />
-                <div className="relative">
-                    <div className="absolute inset-0 flex items-center" aria-hidden="true">
-                        <div className="w-full border-t border-gray-300" />
-                    </div>
-                    <div className="relative flex justify-center">
-                        <span className="bg-white px-3 text-base font-semibold leading-6 text-gray-900">Item Details</span>
-                    </div>
-                </div>
-                <br />
                 {formFields?.inventory_item?.map((form: any) => {
                     return (
+
                         <div key={form.id}>
+                            <div className="relative">
+                                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                                    <div className="w-full border-t border-gray-300" />
+                                </div>
+                                <div className="relative flex justify-center">
+                                    <span className="bg-white px-3 text-base font-semibold leading-6 text-gray-900">Item Details</span>
+                                </div>
+                            </div>
+                            <br />
                             <TextField
                                 label="Item Name"
                                 name="name"
@@ -537,7 +538,7 @@ export function EditForm({ id, orgID }: { id: number, orgID: any }) {
                                 type="text"
                                 placeholder={form.perishable}
                                 onChange={event => handleFormChange(event, form.id)}
-                                value={form.perishable ? "YES":"NO"}
+                                value={form.perishable ? "YES" : "NO"}
                                 readOnly />
                             <br />
                             <br />
