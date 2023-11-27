6daf1b94f79a11abddcc41e72ee55b6323f72cd8
SleepyGabe <gaboparinas78@gmail.com>
2023-11-27 17:59:52 +0800

    rejection testing.

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,6 +7,7 @@ import { getURL } from '@/app/utils/url'
 
 export async function middleware(req: NextRequest) {
   var charity_status: any
+  var rejected: any
   const res = NextResponse.next()
 
   const requestUrl = new URL(req.url)
@@ -32,15 +33,19 @@ export async function middleware(req: NextRequest) {
 
     const { data: charity_member, error: error_2 } = await supabase
       .from('charity_member')
-      .select('*, charity ( id, charity_verified )')
+      .select('*, charity ( id, charity_verified, is_rejected )')
       .eq('user_uuid', uid)
 
     charity_member?.map(member => (
-      charity_status = member.charity.charity_verified
+      charity_status = member.charity.charity_verified,
+      rejected = member.charity.is_rejected
     ))
 
-    if (charity_member?.length !== 1 || charity_status == false) {
+    if (charity_member?.length !== 1) {
       return Response.redirect(requestUrl.origin)
+    }
+    else if (charity_status == false && rejected == false) {
+      return Response.redirect(`${requestUrl.origin}/pending`)
     }
   }
   else if (originalUrl.includes('/settings')) {
