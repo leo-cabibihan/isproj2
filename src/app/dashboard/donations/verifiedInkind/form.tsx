a24b5be8a2cbe0d55b9916bd4cad74ed2b6e0d38
SleepyGabe <gaboparinas78@gmail.com>
2023-12-02 11:28:25 +0800

    minor fix

--- src/app/dashboard/donations/verifiedInkind/form.tsx
+++ src/app/dashboard/donations/verifiedInkind/form.tsx
@@ -437,14 +437,14 @@ export function MultilayeredForm(ID: any) {
             checked={withRemarks}
             onChange={setWithRemarks}
             className={classNames(
-              newDonor ? 'bg-indigo-600' : 'bg-gray-200',
+              withRemarks ? 'bg-indigo-600' : 'bg-gray-200',
               'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2',
             )}
           >
             <span
               aria-hidden="true"
               className={classNames(
-                newDonor ? 'translate-x-5' : 'translate-x-0',
+                withRemarks ? 'translate-x-5' : 'translate-x-0',
                 'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
               )}
             />
