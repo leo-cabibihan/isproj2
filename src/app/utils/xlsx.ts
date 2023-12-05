e0ed2fe936580d01e63850de72a66337ff0ae3c5
SleepyGabe <gaboparinas78@gmail.com>
2023-12-05 20:13:06 +0800

    COPIUM

--- src/app/utils/xlsx.ts
+++ src/app/utils/xlsx.ts
@@ -1,15 +1,23 @@
 import * as XLSX from 'xlsx';
+import * as fs from 'fs';
+import { Readable } from 'stream';
+import * as cpexcel from 'xlsx/dist/cpexcel.full.mjs';
 
 export async function handleTableExport(data: any, filename: any) {
-//THIS GENERATES AN EXCEL WORKSHEET
-const worksheet = XLSX.utils.json_to_sheet(data);
 
-//THIS GENERATES AN EXCEL WORKBOOK
-const workbook = XLSX.utils.book_new();
-XLSX.utils.book_append_sheet(workbook, worksheet, "Exported Data")
+    XLSX.set_fs(fs);
+    XLSX.stream.set_readable(Readable);
+    XLSX.set_cptable(cpexcel);
 
-//THIS WRITES THE EXPORTED FILE
-XLSX.writeFile(workbook, `${filename}.xlsx`, { compression: true })
+    //THIS GENERATES AN EXCEL WORKSHEET
+    const worksheet = XLSX.utils.json_to_sheet(data);
 
-return console.log("FILE EXPORTED SUCCESSFULLY!")
+    //THIS GENERATES AN EXCEL WORKBOOK
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Exported Data")
+
+    //THIS WRITES THE EXPORTED FILE
+    XLSX.writeFile(workbook, `${filename}.xlsx`, { compression: true })
+
+    return console.log("FILE EXPORTED SUCCESSFULLY!")
 }
