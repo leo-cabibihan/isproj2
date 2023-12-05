3ada8689760c6ff839e1943ba418ebc536af9694
SleepyGabe <gaboparinas78@gmail.com>
2023-12-05 20:17:52 +0800

    ...

--- src/app/utils/xlsx.ts
+++ src/app/utils/xlsx.ts
@@ -1,23 +1,15 @@
 import * as XLSX from 'xlsx';
-import * as fs from 'fs'
-import { Readable } from 'stream';
-import * as cpexcel from 'xlsx/dist/cpexcel.full.mjs';
 
 export async function handleTableExport(data: any, filename: any) {
+//THIS GENERATES AN EXCEL WORKSHEET
+const worksheet = XLSX.utils.json_to_sheet(data);
 
-    XLSX.set_fs(fs);
-    XLSX.stream.set_readable(Readable);
-    XLSX.set_cptable(cpexcel);
+//THIS GENERATES AN EXCEL WORKBOOK
+const workbook = XLSX.utils.book_new();
+XLSX.utils.book_append_sheet(workbook, worksheet, "Exported Data")
 
-    //THIS GENERATES AN EXCEL WORKSHEET
-    const worksheet = XLSX.utils.json_to_sheet(data);
+//THIS WRITES THE EXPORTED FILE
+XLSX.writeFile(workbook, `${filename}.xlsx`, { compression: true })
 
-    //THIS GENERATES AN EXCEL WORKBOOK
-    const workbook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Exported Data")
-
-    //THIS WRITES THE EXPORTED FILE
-    XLSX.writeFile(workbook, `${filename}.xlsx`, { compression: true })
-
-    return console.log("FILE EXPORTED SUCCESSFULLY!")
+return console.log("FILE")
 }
--- src/components/SlideOverButton.tsx
+++ src/components/SlideOverButton.tsx
@@ -65,31 +65,31 @@ export function SlideOver({ buttontext, children, variant, color, title }: { but
 
 export function ExportButton({ variant, color, data, filename }: { variant: VariantKey, color: ColorKey, data: any, filename: String }) {
 
-  // console.log("IDK ANYMORE ", data, filename)
-  // console.log("IS IT STRINGIFY??? ", JSON.stringify({ rows: data }))
-  // console.log("THIS IS IN NON-OBJECT ", JSON.stringify(data))
+  console.log("IDK ANYMORE ", data, filename)
+  console.log("IS IT STRINGIFY??? ", JSON.stringify({ rows: data }))
+  console.log("THIS IS IN NON-OBJECT ", JSON.stringify(data))
 
-  // const handleExport = async (e: any) => {
-  //   e.preventDefault()
-  //   const rawResponse = await fetch(
-  //     `https://isproj2.vercel.app/dashboard/donations/cash/post`,
-  //     {
-  //       method: 'POST',
-  //       headers: {
-  //         Accept: 'application/json',
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //         rows: data,
-  //         file_name: filename
-  //       }),
-  //     },
-  //   )
-  // }
+  const handleExport = async (e: any) => {
+    e.preventDefault()
+    const rawResponse = await fetch(
+      `https://isproj2.vercel.app/dashboard/donations/cash/post`,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          rows: data,
+          file_name: filename
+        }),
+      },
+    )
+  }
 
   return (
     <>
-      <Button variant={variant} color={color} onClick={handleTableExport(data, filename)}>Download Data</Button>
+      <Button variant={variant} color={color} onClick={handleExport}>Download Data</Button>
     </>
 
   )
