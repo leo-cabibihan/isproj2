6ca7ea11a4854b5359dd2e681cc3c84035a992c0
SleepyGabe <gaboparinas78@gmail.com>
2023-12-06 18:10:30 +0800

    iiiii

--- src/app/settings/donationHistory/page.tsx
+++ src/app/settings/donationHistory/page.tsx
@@ -109,7 +109,7 @@ export default async function donationHistory() {
                   <div className="mt-8 flow-root">
                     <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                       <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-                        <ExportTest rows={rows} fileName={`YOUR DONATION HISTORY`} sheetName={"HISTORY"} />
+                        <ExportTest rows={rows} fileName={`YOUR DONATION HISTORY`} sheetName={"DONATION HISTORY"} />
                         <table className="min-w-full divide-y divide-gray-300">
                           <thead>
                             <tr>
