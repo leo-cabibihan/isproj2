1c1498905790fba59ff8f649cf27c167e95a30f2
SleepyGabe <gaboparinas78@gmail.com>
2023-12-06 21:20:14 +0800

    scuffed way to fix thankyou redirects.

---
+++ src/app/thank-you/page.tsx
@@ -0,0 +1,48 @@
+// @ts-nocheck 
+'use client'
+import Link from 'next/link'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Button } from '@/components/Button'
+import { Logo } from '@/components/Logo'
+import { SlimLayout } from '@/components/SlimLayout'
+
+export default function ThankYou() {
+  return (
+    <>
+    <div className="grid min-h-full grid-cols-1 grid-rows-[1fr,auto,1fr] bg-white lg:grid-cols-[max(50%,36rem),1fr]">
+        <header className="mx-auto w-full max-w-7xl px-6 pt-6 sm:pt-10 lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:px-8">
+          <a href="#">
+            <span className="sr-only">Your Company</span>
+            <div className="flex">
+              <Link href="/" aria-label="Home">
+                <Logo className="h-10 w-auto" />
+              </Link>
+            </div>
+          </a>
+        </header>
+        <main className="mx-auto w-full max-w-7xl px-6 py-24 sm:py-32 lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:px-8">
+          <div className="max-w-lg">
+            <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Thank you for donating!</h1>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              Thank you for donating!
+            </p>
+            <div className="mt-10">
+              <a href="/" className="text-sm font-semibold leading-7 text-indigo-600">
+                <span aria-hidden="true">&larr;</span> Back to home
+              </a>
+            </div>
+          </div>
+        </main>
+        <div className="hidden lg:relative lg:col-start-2 lg:row-start-1 lg:row-end-4 lg:block">
+          <img
+            src="https://todaysparent.mblycdn.com/uploads/tp/2019/12/header-charity-entertainment.jpg"
+            alt=""
+            className="absolute inset-0 h-full w-full object-cover"
+          />
+        </div>
+      </div>
+      <Footer></Footer>
+      </>
+  )
+} 
