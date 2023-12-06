d9eaaa0ad58006fc7bc085dbb92947428d6a3d58
SleepyGabe <gaboparinas78@gmail.com>
2023-12-06 11:03:19 +0800

    it works?

--- src/components/SlideOverButton.tsx
+++ src/components/SlideOverButton.tsx
@@ -67,7 +67,7 @@ export default function SlideOver({ buttontext, children, variant, color, title 
   )
 }
 
-export function ExportTest() {
+export function ExportTest({rows}: {rows: any}) {
 
   XLSX.stream.set_readable(Readable);
   XLSX.set_cptable(cpexcel);
@@ -77,30 +77,27 @@ export function ExportTest() {
     <>
       <Button variant='solid' color='green' onClick={
         async () => {
-          /* fetch JSON data and parse */
-          const url = "https://sheetjs.com/data/executive.json";
-          const raw_data = await (await fetch(url)).json();
+          // /* fetch JSON data and parse */
+          // const url = "https://sheetjs.com/data/executive.json";
+          // const raw_data = await (await fetch(url)).json();
 
-          /* filter for the Presidents */
-          const prez = raw_data.filter(row => row.terms.some(term => term.type === "prez"));
+          // /* filter for the Presidents */
+          // const prez = raw_data.filter(row => row.terms.some(term => term.type === "prez"));
 
-          /* sort by first presidential term */
-          prez.forEach(row => row.start = row.terms.find(term => term.type === "prez").start);
-          prez.sort((l, r) => l.start.localeCompare(r.start));
+          // /* sort by first presidential term */
+          // prez.forEach(row => row.start = row.terms.find(term => term.type === "prez").start);
+          // prez.sort((l, r) => l.start.localeCompare(r.start));
 
-          /* flatten objects */
-          const rows = prez.map(row => ({
-            name: row.name.first + " " + row.name.last,
-            birthday: row.bio.birthday
-          }));
+          // /* flatten objects */
+          // const rows = prez.map(row => ({
+          //   name: row.name.first + " " + row.name.last,
+          //   birthday: row.bio.birthday
+          // }));
 
           /* generate worksheet and workbook */
           const worksheet = XLSX.utils.json_to_sheet(rows);
           const workbook = XLSX.utils.book_new();
           XLSX.utils.book_append_sheet(workbook, worksheet, "Dates");
-
-          /* fix headers */
-          XLSX.utils.sheet_add_aoa(worksheet, [["Name", "Birthday"]], { origin: "A1" });
 
           /* calculate column width */
           const max_width = rows.reduce((w, r) => Math.max(w, r.name.length), 10);
