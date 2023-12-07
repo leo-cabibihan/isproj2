d0a270bab892470ad2f0de19ec6fc295236f77ca
SleepyGabe <gaboparinas78@gmail.com>
2023-12-07 17:33:25 +0800

    fix.

--- src/app/[id]/details/form.tsx
+++ src/app/[id]/details/form.tsx
@@ -14,8 +14,47 @@ import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js'
 import { PayPal } from '@/components/paypal/CashForm'
 import TestPage from '@/app/paypal/payment'
 import { useRouter } from 'next/navigation'
-import Failure from '@/components/modals/Failure'
-import Success from '@/components/modals/Success'
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/20/solid'
+
+function Failure({ heading, content }: any) {
+  return (
+      <div className="rounded-md bg-red-50 p-4">
+          <div className="flex">
+              <div className="flex-shrink-0">
+                  <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+              </div>
+              <div className="ml-3">
+                  <h3 className="text-sm font-medium text-red-800">{heading}</h3>
+                  <div className="mt-2 text-sm text-red-700">
+                      <p>
+                          {content}
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  )
+}
+
+function Success({ heading, content }: any) {
+  return (
+      <div className="rounded-md bg-green-50 p-4">
+          <div className="flex">
+              <div className="flex-shrink-0">
+                  <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
+              </div>
+              <div className="ml-3">
+                  <h3 className="text-sm font-medium text-green-800">{heading}</h3>
+                  <div className="mt-2 text-sm text-green-700">
+                      <p>
+                          {content}
+                      </p>
+                  </div>
+              </div>
+          </div>
+      </div>
+  )
+}
 
 
 function Message({ content, type, heading }: any) {
